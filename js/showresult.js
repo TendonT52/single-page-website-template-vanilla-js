de76f05a26d85ba9b906940125286278e2c3ae20
Tikhamporn Tepsut <88883551+TikhampornSky@users.noreply.github.com>
2022-04-13 20:45:53 +0700

    Finish All

--- js/showresult.js
+++ js/showresult.js
@@ -5,16 +5,14 @@ export function addElement() {
     var result_block = document.getElementById('contain3') ;
     for (var idx = 0; idx < AnswerGroup.length; idx++) {
         var sub_block = document.createElement('div');
-        sub_block.id = "sub-block" + idx ;
+        sub_block.id = "sub_block" ;
         for (var idx2 = 0; idx2 < AnswerGroup[idx].length; idx2++) {
             var text_node = document.createTextNode(AnswerGroup[idx][idx2]) ;
-            sub_block.appendChild(text_node) ;
+            var sub_sub_block = document.createElement('div');
+            sub_sub_block.id = "sub_sub_block" ;
+            sub_sub_block.appendChild(text_node) ;
+            sub_block.appendChild(sub_sub_block) ;
         }
-        //var lineBreak = document.createElement('br');
-        //sub_block.appendChild(lineBreak.cloneNode()) ;
-        var line_break = document.createTextNode('<br />');
-        sub_block.appendChild(line_break) ;
-        console.log(sub_block) ;
         result_block.appendChild(sub_block) ;
     }
 }
